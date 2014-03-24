9f9c0c84bf1f9e27c92735666830927b7da893ae
Lucas Sampaio <lucas@lsmagalhaes.com>
2014-03-24 18:18:33 -0300

    Error hierarchy updated

--- oidclib/errors.py
+++ oidclib/errors.py
@@ -2,7 +2,15 @@
 from oauthlib.oauth2.rfc6749.errors import OAuth2Error
 
 
-class OpenIDScopeError(OAuth2Error):
+class OIDCError(OAuth2Error):
+    pass
+
+
+class OpenIDScopeError(OIDCError):
     error = 'missing_openid_scope'
     description = ('Authorization request is not a valid OpenID Connect request '
                    'due to the lack of the "openid" scope')
+
+
+class NotOpenIDConnectError(OIDCError):
+    error = 'not_oidc_request'
